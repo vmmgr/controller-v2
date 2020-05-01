0a5961a6518738503fec80c2633528b57fe31e99
yoneyan <github@yoneyan.net>
2020-05-02 01:58:30 +0900

    add file for database test

--- db/db_test.go
+++ db/db_test.go
@@ -2,40 +2,14 @@ package db
 
 import (
 	"fmt"
-	"log"
 	"testing"
 )
 
-func testUserCreate() {
-	db := InitDB()
-
-	db.Create(
-		&User{
-			ID:   "test01",
-			Name: "TestUser",
-			Pass: "Test",
-			Auth: 0,
-			//AdminGroup: []Group{
-			//	{ID: "testgroup1", Name: "Group1"},
-			//},
-		},
-	)
-
-	if err := db.Error; err != nil {
-		db.Rollback()
-	}
-}
-
 func TestInitDatabase(t *testing.T) {
-	fmt.Println("fasf")
-	//fmt.Println(d)
-	//t.Failed()
-
-	fmt.Println("tsat")
+	InitCreateDB()
 }
 
 func TestCreateDatabase(t *testing.T) {
-	//testUserCreate()
 	AddDBUser(User{
 		ID:   "TestUser1",
 		Name: "User1",
@@ -49,5 +23,75 @@ func TestCreateDatabase(t *testing.T) {
 		Pass: "password",
 		Auth: 0,
 	})
-	log.Println(GetAllDBUser())
+
+	AddDBToken(Token{
+		Token: "sdfafsafdsfsafdskjgldsfak",
+	})
+
+	result := AddDBGroup(Group{
+		ID:   "TestGroup1",
+		Name: "Group1",
+		//AdminUser: []User{{ID: "TestUser1"}},
+		Private: false,
+	})
+
+	AddDBUser(User{
+		ID:   "TestUser3",
+		Name: "User3",
+		Pass: "password",
+		//AdminGroup: []Group{{ID: "TestGroup1"}},
+		Auth: 0,
+	})
+
+	fmt.Println(result)
+	fmt.Println("=====User=====")
+	fmt.Println(GetAllDBUser())
+	fmt.Println("=====Group=====")
+	fmt.Println(GetAllDBGroup())
+	fmt.Println("=====Token=====")
+	fmt.Println(GetAllDBToken())
+	fmt.Println("==========")
+}
+
+func TestJoinGroupDatabase(t *testing.T) {
+	db := InitDB()
+	defer db.Close()
+
+	//var user []User
+	var group Group
+	group = Group{ID: "TestGroup1"}
+	//user = append(user, User{ID: "TestUser2"})
+
+	//db.Model(&group).Association("AdminUser").Append(user)
+
+	//UpdateDBGroupUser(Group{
+	//	ID:        "TestGroup1",
+	//	AdminUser: []User{{ID: "TestUser1"}},
+	//})
+	//user1 := User{ID: "TestUser1"}
+	//db.Model(&user1).Association("AdminGroup").Append(Group{Name: "Group1"})
+	fmt.Println("===========")
+	//user2 := User{ID: "TestUser1"}
+	//var group Group
+	//db.Model(&user2).Association("UserGroup").Append(Group{ID: "TestGroup1"})
+	//db.Model(&user2).Association("User").Find(&group)
+	fmt.Println("=====User=====")
+	fmt.Println(GetAllDBUser())
+	fmt.Println("=====Group=====")
+	fmt.Println(GetAllDBGroup())
+	fmt.Println("==========")
+	db.Model(&group).Association("StandardUser").Append(&User{ID: "TestUser1"})
+
+	//UpdateDBGroupUser(Group{
+	//	ID:           "TestGroup1",
+	//	StandardUser: user,
+	//})
+	fmt.Println("=====User=====")
+	fmt.Println(GetAllDBUser())
+	fmt.Println("=====Group=====")
+	fmt.Println(GetAllDBGroup())
+	fmt.Println("==========")
+
+	//db.Model(&group).Related(&user)
+
 }
