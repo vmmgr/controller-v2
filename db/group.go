7c02809b48a24f8e4605685440c4965ae92173de
yoneyan <github@yoneyan.net>
2020-06-18 23:44:45 +0900

    convert uint to int

--- db/group.go
+++ db/group.go
@@ -1,13 +1,12 @@
 package db
 
-import "log"
-
 //Add
 func AddDBGroup(data Group) bool {
 	db := InitDB()
 	defer db.Close()
+	//db.Table("group").CreateTable(&data)
 	db.Create(&data)
-	log.Println(db.Create(&data))
+
 	if err := db.Error; err != nil {
 		db.Rollback()
 		return false
@@ -21,6 +20,7 @@ func DeleteDBGroup(data Group) bool {
 	db := InitDB()
 	defer db.Close()
 	db.Delete(&data)
+
 	if err := db.Error; err != nil {
 		db.Rollback()
 		return false
@@ -33,8 +33,45 @@ func DeleteDBGroup(data Group) bool {
 func UpdateDBGroup(data Group) bool {
 	db := InitDB()
 	defer db.Close()
-	db.Model(&data).Updates(Group{Name: data.Name, Private: data.Private, AdminUser: data.AdminUser, StandardUser: data.StandardUser})
+	db.Model(&data).Updates(Group{Name: data.Name, Private: data.Private})
 
+	if err := db.Error; err != nil {
+		db.Rollback()
+		return false
+	} else {
+		return true
+	}
+}
+
+//Add DBGroupUser
+func AddDBGroupUser(data Group, userID string, admin bool) bool {
+	db := InitDB()
+	defer db.Close()
+
+	if admin {
+		db.Model(&data).Association("AdminUser").Append(&User{ID: userID})
+	} else {
+		db.Model(&data).Association("StandardUser").Append(&User{ID: userID})
+	}
+
+	if err := db.Error; err != nil {
+		db.Rollback()
+		return false
+	} else {
+		return true
+	}
+}
+
+//Delete DBGroupUser
+func DeleteDBGroupUser(data Group, userID string, admin bool) bool {
+	db := InitDB()
+	defer db.Close()
+
+	if admin {
+		db.Model(&data).Association("AdminUser").Delete(&User{ID: userID})
+	} else {
+		db.Model(&data).Association("StandardUser").Delete(&User{ID: userID})
+	}
 	if err := db.Error; err != nil {
 		db.Rollback()
 		return false
@@ -49,6 +86,7 @@ func GetAllDBGroup() []Group {
 	defer db.Close()
 
 	var group []Group
+	//db.Table("group").Find(&group)
 	db.Find(&group)
 	return group
 }
