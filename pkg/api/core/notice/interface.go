f5023e5fb743a1cb44edf92092c6207345a4f750
yoneyan <github@yoneyan.net>
2020-10-26 20:38:15 +0900

    notice関連のファイルの追加

---
+++ pkg/api/core/notice/interface.go
@@ -0,0 +1,43 @@
+package notice
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+const (
+	ID               = 0
+	UserID           = 1
+	GroupID          = 2
+	UserIDAndGroupID = 3
+	Everyone         = 4
+	Data             = 5
+	Important        = 10
+	Fault            = 11
+	Info             = 12
+	UpdateAll        = 110
+)
+
+type Notice struct {
+	gorm.Model
+	UserID     uint   `json:"user_id"`
+	GroupID    uint   `json:"group_id"`
+	Everyone   *bool  `json:"everyone"`
+	StartTime  uint   `json:"start_time"`
+	EndingTime uint   `json:"ending_time"`
+	Important  *bool  `json:"important"`
+	Fault      *bool  `json:"fault"`
+	Info       *bool  `json:"info"`
+	Title      string `json:"title"`
+	Data       string `json:"data"`
+}
+
+type Result struct {
+	Status bool     `json:"status"`
+	Error  string   `json:"error"`
+	Notice []Notice `json:"notice"`
+}
+
+type ResultDatabase struct {
+	Err    error
+	Notice []Notice
+}
---
+++ pkg/api/core/notice/v0/notice.go
@@ -0,0 +1,31 @@
+package v0
+
+import (
+	"github.com/gin-gonic/gin"
+	auth "github.com/vmmgr/controller/pkg/api/core/auth/v0"
+	"github.com/vmmgr/controller/pkg/api/core/notice"
+	"github.com/vmmgr/controller/pkg/api/core/token"
+	dbNotice "github.com/vmmgr/controller/pkg/api/store/notice/v0"
+	"net/http"
+)
+
+func Get(c *gin.Context) {
+	userToken := c.Request.Header.Get("USER_TOKEN")
+	accessToken := c.Request.Header.Get("ACCESS_TOKEN")
+
+	// Group authentication
+	result := auth.GroupAuthentication(token.Token{UserToken: userToken, AccessToken: accessToken})
+	if result.Err != nil {
+		c.JSON(http.StatusInternalServerError, notice.Result{Status: false, Error: result.Err.Error()})
+		return
+	}
+
+	noticeResult := dbNotice.Get(notice.Data, &notice.Notice{UserID: result.User.ID, GroupID: result.Group.ID,
+		Everyone: &[]bool{true}[0]})
+	if noticeResult.Err != nil {
+		c.JSON(http.StatusInternalServerError, notice.Result{Status: false, Error: result.Err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, notice.Result{Status: true, Notice: noticeResult.Notice})
+}
