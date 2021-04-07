b1871ca2a3c2a48f0a85456f0a61e2efed96b86f
yoneyan <github@yoneyan.net>
2021-04-07 19:31:36 +0900

    [add] ssh test file

---
+++ pkg/api/core/node/image/v0/image_test.go
@@ -0,0 +1,80 @@
+package v0
+
+import (
+	"github.com/pkg/sftp"
+	"github.com/schollz/progressbar"
+	"github.com/vmmgr/controller/pkg/api/core/tool/config"
+	"github.com/vmmgr/controller/pkg/api/core/tool/ssh"
+	"io"
+	"log"
+	"net/http"
+	"testing"
+	"time"
+)
+
+//var url = "https://releases.ubuntu.com/20.04.2.0/ubuntu-20.04.2.0-desktop-amd64.iso"
+var url = "http://tinycorelinux.net/12.x/x86/release/TinyCore-current.iso"
+var dstPath = "/home/ubuntu/test.iso"
+var nodeID uint = 2
+
+func TestConfigApply(t *testing.T) {
+	if err := config.GetConfig("../../../../../../cmd/backend/con.json"); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestImageDownload(t *testing.T) {
+
+	conn, err := ssh.ConnectSSH(nodeID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer conn.Close()
+
+	// SFTP Client
+	client, err := sftp.NewClient(conn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer client.Close()
+
+	// dstFileの作成(ssh先)
+	dstFile, err := client.Create(dstPath)
+	if err != nil {
+		log.Println(err)
+	}
+	defer dstFile.Close()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	t.Log(resp.ContentLength)
+
+	p := Progress{total: resp.ContentLength}
+
+	count := 100
+	count64 := int64(count)
+	bar := progressbar.Default(count64)
+
+	// Node側の表示
+	go func() {
+		for {
+			if p.size != p.total {
+				<-time.NewTimer(200 * time.Microsecond).C
+				bar.Set(int(float64(p.size) / float64(p.total) * 100))
+			} else {
+				return
+			}
+		}
+	}()
+	// Write the body to file
+
+	_, err = io.Copy(dstFile, io.TeeReader(resp.Body, &p))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
---
+++ pkg/api/core/tool/ssh/ssh_test.go
@@ -0,0 +1,85 @@
+package ssh
+
+import (
+	"bytes"
+	"github.com/jinzhu/gorm"
+	"github.com/vmmgr/controller/pkg/api/core"
+	"github.com/vmmgr/controller/pkg/api/core/node"
+	"github.com/vmmgr/controller/pkg/api/core/tool/config"
+	dbNode "github.com/vmmgr/controller/pkg/api/store/node/v0"
+	"golang.org/x/crypto/ssh"
+	"strconv"
+	"testing"
+)
+
+var nodeID uint = 2
+
+func TestConfigApply(t *testing.T) {
+	if err := config.GetConfig("../../../../../cmd/backend/con.json"); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSSHClient(t *testing.T) {
+	resultNode := dbNode.Get(node.ID, &core.Node{Model: gorm.Model{ID: nodeID}})
+	if resultNode.Err != nil {
+		t.Fatal(resultNode.Err)
+	}
+
+	//t.Log(resultNode.Node[0])
+
+	var config *ssh.ClientConfig
+
+	ip := resultNode.Node[0].IP
+	port := strconv.Itoa(int(resultNode.Node[0].Port))
+	user := resultNode.Node[0].UserName
+	pass := resultNode.Node[0].Password
+
+	// 鍵認証
+	if *resultNode.Node[0].UseKey {
+		t.Log("UseKey")
+		key, err := ssh.ParsePrivateKey([]byte(resultNode.Node[0].PublicKey))
+		if err != nil {
+			panic(err)
+		}
+
+		config = &ssh.ClientConfig{
+			User:            user,
+			HostKeyCallback: ssh.InsecureIgnoreHostKey(), // https://github.com/golang/go/issues/19767
+			Auth: []ssh.AuthMethod{
+				ssh.PublicKeys(key),
+			},
+		}
+	} else {
+		t.Log("UsePassword")
+		//Passフレーズ認証
+		config = &ssh.ClientConfig{
+			User:            user,
+			HostKeyCallback: ssh.InsecureIgnoreHostKey(), // https://github.com/golang/go/issues/19767
+			Auth: []ssh.AuthMethod{
+				ssh.Password(pass),
+			},
+		}
+
+	}
+
+	conn, err := ssh.Dial("tcp", ip+":"+port, config)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+
+	session, err := conn.NewSession()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer session.Close()
+	//Check whoami
+	var b bytes.Buffer
+	session.Stdout = &b
+	remoteCommand := "whoami"
+	if err = session.Run(remoteCommand); err != nil {
+		t.Fatal("Failed to run: " + err.Error())
+	}
+	t.Log(remoteCommand + ":" + b.String())
+}
